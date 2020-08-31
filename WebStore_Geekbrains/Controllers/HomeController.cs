5b397cdd2c006e1005bba0e2b84d0beea6ef199a
Евгений ★ <zhenya-ski@mail.ru>
2020-08-31 22:52:42 +0900

    дополнение

--- WebStore_Geekbrains/Controllers/HomeController.cs
+++ WebStore_Geekbrains/Controllers/HomeController.cs
@@ -6,15 +6,28 @@ using Microsoft.AspNetCore.Mvc;
 
 namespace WebStore_Geekbrains.Controllers
 {
-    public class HomeController : Controller 
+    public class HomeController : Controller
     {
-        
+
         public IActionResult Index()
         {
             return View();
         }
 
         public IActionResult Blog()
+        {
+            return View();
+        }
+
+        public IActionResult Shop()
+        {
+            return View();
+        }
+        public IActionResult Error404()
+        {
+            return View();
+        }    
+        public IActionResult BlogSingle()
         {
             return View();
         }
