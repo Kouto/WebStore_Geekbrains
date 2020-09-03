97d3268efdaefd4ebfb74fd9b692f5f79a3084c8
Евгений ★ <zhenya-ski@mail.ru>
2020-09-03 23:05:55 +0900

    Работа над ДЗ3, поиск ошибки

---
+++ WebStore_Geekbrains/Controllers/EmployeeController.cs
@@ -0,0 +1,38 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Mvc;
+using WebStore_Geekbrains.Interfaces.Infrastructure;
+using WebStore_Geekbrains.ViewModels;
+
+namespace WebStore_Geekbrains.Controllers
+{
+    //[Route("users/[action]")]
+    [Route("users")]
+    public class EmployeeController : Controller
+    {
+        private readonly IEmployeesService _employeesService;
+        public EmployeeController(IEmployeesService employeesService)
+        {
+            _employeesService = employeesService;
+        }
+        [Route("all")]
+        public IActionResult Employees()
+        {
+            return View(_employeesService.GetAll());
+        }
+        [Route("id")]
+        public IActionResult EmployeeDetails(int id)
+        {
+            var employee = _employeesService.GetById(id);
+
+            if (employee==null)
+            {
+                return NotFound();
+            }
+
+            return View(employee);
+        }
+    }
+}
--- WebStore_Geekbrains/ViewModels/EmployeesViewModel.cs
+++ WebStore_Geekbrains/ViewModels/EmployeesViewModel.cs
@@ -1,10 +1,4 @@
-﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Threading.Tasks;
-using Microsoft.AspNetCore.Mvc;
-
-namespace WebStore_Geekbrains.ViewModels
+﻿namespace WebStore_Geekbrains.ViewModels
 {
     public class EmployeeViewModel
     {
